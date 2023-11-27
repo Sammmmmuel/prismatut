ab1f8dea2c4145ea056569fe6c17722c8b7236d9
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2023-11-26 19:21:13 -0800

    createMany function working

--- script.ts
+++ script.ts
@@ -2,16 +2,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.create({
-        data: {
+    await prisma.user.deleteMany()
+    const users = await prisma.user.createMany({
+        data: [{
             name: "sam",
             email: "sam@test.com",
-            age: 21
-        }
+            age: 21,
+        },{
+            name: "sean",
+            email: "sean@test.com",
+            age: 27,
+        }]
     })
-    
-    console.log(user)
+    console.log(users)
 }
+
+
 main()
     .catch(e => {
         console.error(e.message)
